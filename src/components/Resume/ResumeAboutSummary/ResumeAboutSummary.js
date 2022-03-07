6d0db48ed2f5934f8f74ae9eb99f982306457557
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2022-03-06 22:31:36 -0600

    Update node version and library dependencies to latest versions

--- src/components/Resume/ResumeAboutSummary/ResumeAboutSummary.js
+++ src/components/Resume/ResumeAboutSummary/ResumeAboutSummary.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import marked from 'marked';
+import { marked } from 'marked';
 import { sanitize } from 'dompurify';
 
 import ResumeSection from '../ResumeSection/ResumeSection';
